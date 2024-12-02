7c7e71aff766997db70bc511a4bd89174b1db462
maiyo008 <tonymaiyo008@gmail.com>
2024-12-02 18:42:33 +0300

    code clean up

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,41 @@
+-- Query 1: INNER JOIN - Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email
+FROM 
+    Booking
+INNER JOIN 
+    User
+ON 
+    Booking.user_id = User.user_id;
+
+-- Query 2: LEFT JOIN - Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    Property.property_id,
+    Property.name AS property_name,
+    Property.location,
+    Property.price_per_night,
+    Review.review_id,
+    Review.rating,
+    Review.comment,
+    Review.created_at AS review_date
+FROM 
+    Property
+LEFT JOIN 
+    Review
+ON 
+    Property.property_id = Review.property_id;
+
+
+
 -- Query 3: FULL OUTER JOIN - Retrieve all users and all bookings, even if a user has no booking or a booking is not linked to a user
 SELECT 
     User.user_id AS user_id,
--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,4 +1,24 @@
 SELECT 
+    property_id, 
+    name AS property_name, 
+    location, 
+    price_per_night
+FROM 
+    Property
+WHERE 
+    property_id IN (
+        SELECT 
+            property_id
+        FROM 
+            Review
+        GROUP BY 
+            property_id
+        HAVING 
+            AVG(rating) > 4.0
+    );
+
+
+SELECT 
     user_id, 
     first_name, 
     last_name, 
